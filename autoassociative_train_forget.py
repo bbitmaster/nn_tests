5b3c6017f51b08a02f1d69d7fb8eca849dc9313d
Ben Goodrich <bbitmaster@gmail.com>
2013-11-17 19:37:40 -0500

    updated autoassociative trainer

--- autoassociative_train_forget.py
+++ autoassociative_train_forget.py
@@ -44,20 +44,27 @@ for i in range(2,len(sys.argv)):
 
 np.random.seed(p['random_seed']);
 
-sample_size = 100;
-num_old_samples = 100;
-num_new_samples = 100;
-
-old_sample_data = np.random.randint(0,2,(num_old_samples,sample_size))
+sample_size = p['sample_size'];
+num_old_samples = p['num_old_samples'];
+num_new_samples = p['num_new_samples'];
 
 new_sample_data = np.random.randint(0,2,(num_new_samples,sample_size))
+old_sample_data = np.random.randint(0,2,(num_old_samples,sample_size))
 
+new_sample_targets = np.random.randint(0,2,(num_new_samples,sample_size))
+old_sample_targets = np.random.randint(0,2,(num_old_samples,sample_size))
+
+if(p['zerosandones'] == False):
+    old_sample_data = (old_sample_data-0.5)*2.0
+    new_sample_data = (new_sample_data-0.5)*2.0
+    old_sample_targets = (old_sample_targets-0.5)*2.0
+    new_sample_targets = (new_sample_targets-0.5)*2.0
 
 num_hidden = p['num_hidden']
 
 training_epochs = p['training_epochs']
 
-minibatch_size = 10;
+minibatch_size = p['minibatch_size'];
 
 layers = [];
 layers.append(nnet.layer(sample_size))
@@ -65,22 +72,25 @@ layers.append(nnet.layer(p['num_hidden'],p['activation_function'],select_func=p[
                          select_func_params=p['num_selected_neurons'],
                          initialization_scheme=p['initialization_scheme'],
                          initialization_constant=p['initialization_constant'],
-                         dropout=p['dropout']))
-
+                         dropout=p['dropout'],sparse_penalty=p['sparse_penalty'],
+                         sparse_target=p['sparse_target']))
+                         
 #Add 2nd and 3rd hidden layers if there are parameters indicating that we should
 if(p.has_key('num_hidden2')):
     layers.append(nnet.layer(p['num_hidden2'],p['activation_function2'],select_func=p['select_func2'],
                              select_func_params=p['num_selected_neurons2'],
                              initialization_scheme=p['initialization_scheme2'],
                              initialization_constant=p['initialization_constant2'],
-                             dropout=p['dropout2']))
+                             dropout=p['dropout2'],sparse_penalty=p['sparse_penalty2'],
+                             sparse_target=p['sparse_target2']))
 
 if(p.has_key('num_hidden3')):
     layers.append(nnet.layer(p['num_hidden3'],p['activation_function3'],select_func=p['select_func3'],
                              select_func_params=p['num_selected_neurons3'],
                              initialization_scheme=p['initialization_scheme3'],
                              initialization_constant=p['initialization_constant3'],
-                             dropout=p['dropout3']))
+                             dropout=p['dropout3'],sparse_penalty=p['sparse_penalty3'],
+                             sparse_target=p['sparse_target3']))
 
 layers.append(nnet.layer(sample_size,p['activation_function_final']))
 
@@ -95,56 +105,100 @@ save_interval = p['save_interval']
 save_time = time.time()
 
 #these are the variables to save
-train_mse_list = [];
+test_mse_old_list = []
+test_accuracy_old_list = []
+test_mse_new_list = []
+test_accuracy_new_list = []
 
 sample_data = old_sample_data;
+sample_targets = old_sample_targets
 num_samples = num_old_samples;
 
 #comment/uncomment to make old data contain new
-sample_data = np.append(new_sample_data,old_sample_data,axis=0)
-num_samples = num_old_samples + num_new_samples;
+#sample_data = np.append(new_sample_data,old_sample_data,axis=0)
+#sample_targets = np.append(new_sample_targets,old_sample_targets,axis=0)
+#num_samples = num_old_samples + num_new_samples;
 
 for i in range(training_epochs):
     
     if(i == p['forget_epoch']):
         sample_data = new_sample_data;
+        sample_targets = new_sample_targets
         num_samples = num_new_samples;
         print('shuffling')
     minibatch_count = int(num_samples/minibatch_size)
     
     #shuffle data
+    rng_state = np.random.get_state();
     np.random.shuffle(sample_data)
+    np.random.set_state(rng_state)
+    np.random.shuffle(sample_targets)
     
     #count number of correct
 
     for j in range(minibatch_count+1):
         #grab a minibatch
         net.input = np.transpose(sample_data[j*minibatch_size:(j+1)*minibatch_size])
-        target = np.transpose(sample_data[j*minibatch_size:(j+1)*minibatch_size])
+        target = np.transpose(sample_targets[j*minibatch_size:(j+1)*minibatch_size])
         net.feed_forward()
         net.error = net.output - target
         net.back_propagate()
         net.update_weights()
         
-    #get test error rate
+    #get old data test error rate and accuracy
     net.train=False
-    train_mse = 0;
     net.input = np.transpose(old_sample_data)
     net.feed_forward()
-    net.error = net.output - np.transpose(old_sample_data)
-    train_mse = train_mse + (np.sum(net.error**2)/num_old_samples)
+    net.error = net.output - np.transpose(old_sample_targets)
+    test_mse_old = (np.sum(net.error**2)/num_old_samples)
+    if(p['zerosandones']):
+        threshold = 0.5
+    else:
+        threshold = 0.0
+    binary_output = net.output > threshold;
+    binary_output = binary_output.astype(np.float64)
+    if(p['zerosandones'] == False):
+        binary_output = (binary_output-0.5)*2.0
+    accuracy = np.abs(binary_output - np.transpose(old_sample_targets))
+    accuracy = accuracy > .0001
+    test_accuracy_old = 1.0 - np.float64(np.sum(accuracy))/(num_old_samples*sample_size)
+    net.train=True
+
+    #get new data test error rate and accuracy
+    net.train=False
+    net.input = np.transpose(new_sample_data)
+    net.feed_forward()
+    net.error = net.output - np.transpose(new_sample_targets)
+    test_mse_new = (np.sum(net.error**2)/num_new_samples)
+    if(p['zerosandones']):
+        threshold = 0.5
+    else:
+        threshold = 0.0
+    binary_output = net.output > threshold;
+    binary_output = binary_output.astype(np.float64)
+    if(p['zerosandones'] == False):
+        binary_output = (binary_output-0.5)*2.0
+    accuracy = np.abs(binary_output - np.transpose(new_sample_targets))
+    accuracy = accuracy > .0001
+    test_accuracy_new = 1.0 - np.float64(np.sum(accuracy))/(num_new_samples*sample_size)
     net.train=True
 
     #log everything for saving
-    train_mse_list.append(train_mse)
-
+    test_mse_old_list.append(test_mse_old)
+    test_accuracy_old_list.append(test_accuracy_old)
+    test_mse_new_list.append(test_mse_new)
+    test_accuracy_new_list.append(test_accuracy_new)
     
-    print('epoch ' + str(i) + ": " + " train mse: " + str(train_mse));
+    print('epoch ' + str(i) + ": " + " mse_old: " + str(test_mse_old) + " acc_old: " + str(test_accuracy_old)
+    + " mse_new: " + str(test_mse_new) + " acc_new: " + str(test_accuracy_new))
 
     if(time.time() - save_time > save_interval or i == training_epochs-1):
         print('saving results...')
         f_handle = h5py.File(p['results_dir'] + p['simname'] + p['version'] + '.h5py','w')
-        f_handle['train_mse_list'] = np.array(train_mse_list);
+        f_handle['train_mse_old_list'] = np.array(test_mse_old_list);
+        f_handle['test_accuracy_old_list'] = np.array(test_accuracy_old_list);
+        f_handle['train_mse_new_list'] = np.array(test_mse_new_list);
+        f_handle['test_accuracy_new_list'] = np.array(test_accuracy_new_list);
         
         #iterate through all parameters and save them in the parameters group
         p_group = f_handle.create_group('parameters');
--- autoassociative_train_forget_params.py
+++ autoassociative_train_forget_params.py
@@ -3,37 +3,54 @@ from nnet_toolkit import select_funcs as sf;
 
 #A handy name for this run. The data file will be given this name as
 #<resultsdir><simname><version>.h5py
-simname = 'mnist_train_forget_2layer'
+simname = 'autoassociative_train_forget'
 version = '1.1'
 results_dir = '../results/'
 
 data_dir = '../data/'
 
+#tells if the binary values are 0,1 (True) or -1,1 (False)
+zerosandones = False
+
 num_hidden = 300
 num_hidden2 = 300
+num_hidden3 = 300
 
 activation_function='tanh'
 activation_function2='tanh'
-activation_function3='linear_rectifier'
+activation_function3='tanh'
+activation_function_final='tanh'
 
-activation_function_final='sigmoid'
+sparse_penalty=None
+sparse_target=None
 
-learning_rate = .01
+sparse_penalty2=None
+sparse_target2=None
+
+sparse_penalty3=None
+sparse_target3=None
+
+sample_size = 100
+num_old_samples = 100
+num_new_samples = 100
+minibatch_size=10
+
+learning_rate = .001
 training_epochs = 3000
 
-forget_epoch = 1500
+forget_epoch = 1000
 
-num_selected_neurons=100
+num_selected_neurons=50
 select_func = None
 
-num_selected_neurons2=100
+num_selected_neurons2=50
 select_func2 = None
 
-num_selected_neurons3=150
+num_selected_neurons3=50
 select_func3 = None
 
-dropout=0.5
-dropout2=0.5
+dropout=None
+dropout2=None
 dropout3=None
 
 initialization_scheme='glorot'
@@ -44,6 +61,6 @@ initialization_constant=1.0
 initialization_constant2=1.0
 initialization_constant3=1.0
 
-random_seed = 4;
+random_seed = 5;
 
 save_interval = 10*60; #save every 30 minutes
