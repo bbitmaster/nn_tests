6ab2b4deefa747c9de09296956fcb3a3eb8b8f41
Ben Goodrich <bbitmaster@gmail.com>
2014-01-31 14:07:16 -0500

    Updated cluster_simple_test to add P1 and P2 switching

---
+++ cluster_simple_test.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+
+import sys
+
+from nnet_toolkit import nnet
+
+import numpy as np
+import matplotlib.cm as cm
+import image_plotter
+import cluster_select_func as csf
+from autoconvert import autoconvert
+
+#Get the parameters file from the command line
+#use mnist_train__forget_params.py by default (no argument given)
+if(len(sys.argv) > 1):
+        params_file = sys.argv[1]
+else:
+        params_file = 'cluster_simple_test_params.py'
+
+p = {}
+execfile(params_file,p)
+
+#grab extra parameters from command line
+for i in range(2,len(sys.argv)):
+    (k,v) = sys.argv[i].split('=')
+    v = autoconvert(v)
+    p[k] = v
+    print(str(k) + ":" + str(v))
+np.random.seed(p['random_seed'])
+
+examples_per_class = p['examples_per_class'];
+spread = p['spread']
+
+dump_to_file = p['dump_to_file']
+frameskip = p['frameskip']
+img_width = p['img_width'];
+img_height = p['img_height'];
+
+#the axis for the view
+vx_axis = [p['axis_x_min'],p['axis_x_max']];
+vy_axis = [p['axis_y_min'],p['axis_y_max']];
+
+num_classes = 2
+num_hidden = p['num_hidden']
+
+training_epochs = p['training_epochs']
+total_epochs = p['total_epochs']
+
+#minibatch_size = p['minibatch_size']
+
+layers = [];
+layers.append(nnet.layer(2))
+layers.append(nnet.layer(p['num_hidden'],p['activation_function'],
+              dropout=p['dropout'],sparse_penalty=p['sparse_penalty'],
+              sparse_target=p['sparse_target'],use_float32=p['use_float32'],
+              momentum=p['momentum'],maxnorm=p['maxnorm'],step_size=p['learning_rate']))
+
+layers.append(nnet.layer(num_classes,p['activation_function_final'],use_float32=p['use_float32'],
+              momentum=p['momentum_final'],step_size=p['learning_rate_final']))
+
+#init net
+net = nnet.net(layers)
+
+if(p.has_key('cluster_func') and p['cluster_func'] is not None):
+    net.layer[0].centroids = np.asarray((((np.random.random((net.layer[0].weights.shape)) - 0.5)*2.0)),np.float32)
+    net.layer[0].select_func = csf.select_names[p['cluster_func']]
+    net.layer[0].centroid_speed = p['cluster_speed']
+    net.layer[0].num_selected = p['clusters_selected']
+
+#Generate Random Classes
+sample_data1 = np.zeros([num_classes,num_classes*examples_per_class])
+class_data1 = np.ones([num_classes,num_classes*examples_per_class])*p['incorrect_target']
+
+center_x_list1 = [0.7,0.7];
+center_y_list1 = [0.7,-0.7]
+for i in range(num_classes):
+    center_x = center_x_list1[i];
+    center_y = center_y_list1[i];
+    c = np.random.randn(2,examples_per_class)*spread
+    c[0,:] += center_x;
+    c[1,:] += center_y
+    sample_data1[0:2,i*examples_per_class:(i+1)*examples_per_class] = c 
+    class_data1[i,i*examples_per_class:(i+1)*examples_per_class] = 1.0
+
+sample_data2 = np.zeros([num_classes,num_classes*examples_per_class])
+class_data2 = np.ones([num_classes,num_classes*examples_per_class])*p['incorrect_target']
+
+center_x_list2 = [-0.7,-0.7];
+center_y_list2 = [-0.7,0.7]
+for i in range(num_classes):
+    center_x = center_x_list2[i];
+    center_y = center_y_list2[i];
+    c = np.random.randn(2,examples_per_class)*spread
+    c[0,:] += center_x;
+    c[1,:] += center_y
+    sample_data2[0:2,i*examples_per_class:(i+1)*examples_per_class] = c 
+    class_data2[i,i*examples_per_class:(i+1)*examples_per_class] = 1.0
+
+#build a pallete
+pal = cm.rainbow(np.linspace(0,1,num_classes));
+pal = pal[0:num_classes,0:3]
+
+plt = image_plotter.image_plotter();
+plt.initImg(img_width,img_height);
+plt.setAxis(vx_axis[0],vx_axis[1],vy_axis[0],vy_axis[1])
+c=pal[np.argmax(class_data1,0),:]
+
+if(not dump_to_file):
+    plt.show();
+
+
+
+epoch = 1;
+
+while(epoch < p['total_epochs']):
+
+    if(epoch < p['forget_epochs']):
+        train_sample_data = sample_data1
+        train_class_data = class_data1
+        train_mode = 0
+    else:
+        train_sample_data = sample_data2
+        train_class_data = class_data2
+        train_mode = 1
+
+    net.input = train_sample_data;
+    net.feed_forward()
+    net.error = net.output - train_class_data
+    neterror = net.error
+    net_classes = net.output
+
+    net.back_propagate()
+    net.update_weights()
+    if(p['cluster_func'] is not None):
+        csf.update_names[p['cluster_func']](net.layer[0])
+    
+    #get class 1 error rate
+    net.input = sample_data1;
+    net.feed_forward()
+    net.error = net.output - class_data1
+    neterror1 = net.error
+    net_classes1 = net.output
+    num_correct1 = sum(np.equal(np.argmax(net_classes1,0),np.argmax(class_data1,0)))
+    percent_correct1 = num_correct1/float(2*examples_per_class)
+    percent_miss1 = 1.0 - percent_correct1;
+
+    #get class 2 error rate
+    net.input = sample_data2;
+    net.feed_forward()
+    net.error = net.output - class_data2
+    neterror2 = net.error
+    net_classes2 = net.output
+    num_correct2 = sum(np.equal(np.argmax(net_classes2,0),np.argmax(class_data2,0)))
+    percent_correct2 = num_correct2/float(2*examples_per_class)
+    percent_miss2 = 1.0 - percent_correct2;
+
+    if(dump_to_file or epoch%frameskip == 0):
+        xv, yv = np.meshgrid(np.linspace(vx_axis[0],vx_axis[1],img_width),np.linspace(vy_axis[0],vy_axis[1],img_height))
+        xv = np.reshape((xv),(img_height*img_width))
+        yv = np.reshape((yv),(img_height*img_width))
+        net.input = np.vstack((xv,yv))
+        net.feed_forward()
+        img_data=pal[np.argmax(net.output,0),:]
+        img_data = img_data.reshape((img_height,img_width,3))
+        plt.setImg(img_data)
+
+        #class 1
+        #draw incorrect as black, correct as white
+        correct_pal1 = np.array([[0,0,0],[1,1,1]])
+        correct = np.equal(np.argmax(net_classes1,0),np.argmax(class_data1,0))
+        c = correct_pal1[np.int32(correct),:]
+        plt.drawPoint(sample_data1[0,:],sample_data1[1,:],size=1,color=c)
+
+        #class 2
+        correct_pal2 = np.array([[0.1,0.1,0.0],[1.0,1.0,0.9]])
+        correct = np.equal(np.argmax(net_classes2,0),np.argmax(class_data2,0))
+        c = correct_pal2[np.int32(correct),:]
+        plt.drawPoint(sample_data2[0,:],sample_data2[1,:],size=1,color=c)
+
+        #draw centroids
+        if(p['cluster_func'] is not None):
+            plt.drawPoint(net.layer[0].centroids[:,0],net.layer[0].centroids[:,1],size=2,color=(1.0,1.0,0.8))
+
+        #draw dot-product = 0 lines
+        x1 = np.zeros(num_hidden);
+        y1 = np.zeros(num_hidden);
+        x2 = np.zeros(num_hidden);
+        y2 = np.zeros(num_hidden);
+
+        for i in range(num_hidden):
+            #get m and b for y = mx + b
+            m = -net.layer[0].weights[i,0]/net.layer[0].weights[i,1];
+            b = net.layer[0].weights[i,2]/net.layer[0].weights[i,1]
+            #if slope is large, compute x = (y - b)/m
+            if(np.abs(m) > 1):
+                y1[i] = vy_axis[0];
+                x1[i] = (vy_axis[0] - b)/m
+                y2[i] = vy_axis[1];
+                x2[i] = (vy_axis[1] - b)/m
+            else:
+                x1[i] = vx_axis[0];
+                y1[i] = m*vx_axis[0] + b;
+                x2[i] = vx_axis[1];
+                y2[i] = m*vx_axis[1] + b;
+        plt.drawLine(x1,x2,y1,y2,color=(0,0,0))
+        plt.drawRect(0,620,0,35,color=(1,1,1),use_image_coords=True)
+        plt.drawText(1,1,"epoch: " + str(epoch),color=(0,0,0),use_image_coords=True)
+        plt.drawText(120,1,"P1 Miss percent: " + str(percent_miss1),color=(0,0,0),use_image_coords=True)
+        plt.drawText(420,1,"P1 MSE: " + str(np.sum(neterror1**2)),color=(0,0,0),use_image_coords=True)
+        plt.drawText(120,18,"P2 Miss percent: " + str(percent_miss2),color=(0,0,0),use_image_coords=True)
+        plt.drawText(420,18,"P2 MSE: " + str(np.sum(neterror2**2)),color=(0,0,0),use_image_coords=True)
+
+        if(train_mode == 0):
+            plt.drawText(1,18,"P1 Training ",color=(0.5,0.5,0.0),use_image_coords=True)
+        else:
+            plt.drawText(1,18,"P2 Training",color=(0.5,0.5,0.0),use_image_coords=True)
+        plt.drawRect(100,190,340,360,color=(1,1,1),use_image_coords=True)
+        plt.drawText(100,341,"P2 Data",color=(0.0,0.0,0.0),use_image_coords=True)
+        
+        plt.drawRect(450,540,340,360,color=(1,1,1),use_image_coords=True)
+        plt.drawText(450,341,"P1 Data",color=(0.0,0.0,0.0),use_image_coords=True)
+    print("epoch: " + str(epoch) + "percent: " + str(percent_miss1))
+    if(dump_to_file):
+        plt.save_plot(dump_path + "nn_dump" + str(epoch) + ".png")
+    else:
+        if(epoch%frameskip == 0):
+            plt.update()
+    keypress = plt.processEvents();
+    if(keypress == 27):
+        break
+    epoch = epoch + 1;
+
+
+
---
+++ cluster_simple_test_params.py
@@ -0,0 +1,62 @@
+#mnist_train_params
+from nnet_toolkit import select_funcs as sf;
+
+#A handy name for this run. The data file will be given this name as
+#<resultsdir><simname><version>.h5py
+simname = 'mnist_train_nonstationary'
+version = '1.1'
+results_dir = '../results/'
+
+data_dir = '../data/'
+
+use_float32=True
+
+incorrect_target = -1.0
+correct_target = 1.0
+
+examples_per_class = 30
+spread = 0.2
+dump_to_file = False
+frameskip = 25;
+
+img_width = 720
+img_height = 360
+
+axis_x_min = -1.5
+axis_y_min = -1.5
+
+axis_x_max = 1.5
+axis_y_max = 1.5
+
+num_hidden = 64
+
+activation_function='tanh'
+activation_function_final='tanh'
+
+momentum=0.0
+momentum_final = 0.0
+
+maxnorm=None
+
+sparse_penalty=None
+sparse_target=None
+
+#per layer learning rates
+learning_rate =       0.001
+learning_rate_final = 0.001
+
+forget_epochs = 1000
+training_epochs = 3000
+total_epochs = 3000
+
+#can be normal or weighted
+#cluster_func = 'cluster_func'
+cluster_func = None
+cluster_speed = 0.05
+clusters_selected = 4
+
+dropout=None
+
+random_seed = 4;
+
+save_interval = 10*60; #save every 30 minutes
