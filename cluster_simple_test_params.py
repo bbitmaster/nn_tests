bd7dfcc9c06657a75276edf0c80efe963f75af17
Benjamin Fredrick Goodrich <bgoodric@hydra5.eecs.utk.edu>
2014-02-17 21:16:01 -0500

    added random initial clusters to cluster_simple_test

--- cluster_simple_test_params.py
+++ cluster_simple_test_params.py
@@ -25,16 +25,47 @@ correct_target = 1.0
 
 num_classes = 6;
 
-skew_angle = 180./num_classes/2.
+def get_centers_from_angles(num_classes):
+    skew_angle = 180./num_classes/2.
 
-angles1 = np.array(range(0,6))/12.0 * 360 + skew_angle
+    angles1 = np.array(range(0,6))/12.0 * 360 + skew_angle
 
-center_x_list1 = np.sin(angles1*np.pi/180.)*0.7
-center_y_list1 = np.cos(angles1*np.pi/180.)*0.7
+    center_x_list1 = np.sin(angles1*np.pi/180.)*0.7
+    center_y_list1 = np.cos(angles1*np.pi/180.)*0.7
 
-angles2 = (np.array(range(0,6)) + 6.)/12.0 * 360 + skew_angle
-center_x_list2 = np.sin(angles2*np.pi/180.)*0.7
-center_y_list2 = np.cos(angles2*np.pi/180.)*0.7
+    angles2 = (np.array(range(0,6)) + 6.)/12.0 * 360 + skew_angle
+    center_x_list2 = np.sin(angles2*np.pi/180.)*0.7
+    center_y_list2 = np.cos(angles2*np.pi/180.)*0.7
+    return (center_x_list1,center_y_list1,center_x_list2,center_y_list2)
+
+
+def gen_random_centers(num_classes,num_tries,threshold):
+    center_x_list = []
+    center_y_list = []
+    for i in range(num_classes*2):
+        j = 0
+        while j < num_tries:
+            #init between -0.7 and 0.7
+            (center_x_tmp,center_y_tmp) = (np.random.random(2)*2.0 - 1.0)*0.7
+            dist = ((np.array(center_x_list) - center_x_tmp)**2 + (np.array(center_y_list) - center_y_tmp)**2)
+            #print(str(dist))
+            #print(str(dist > threshold**2))
+            if(np.sum(dist < threshold**2) < 1):
+                break
+            j = j + 1
+        print('j ' + str(j) + ' ' + str(center_x_tmp) + ' ' + str(center_y_tmp))
+        center_x_list.append(center_x_tmp)
+        center_y_list.append(center_y_tmp)
+    center_x_list1 = center_x_list[0:num_classes]
+    center_x_list2 = center_x_list[num_classes:(num_classes*2)]
+    center_y_list1 = center_y_list[0:num_classes]
+    center_y_list2 = center_y_list[num_classes:(num_classes*2)]
+
+    #print(str(len(center_x_list)) + ' ' + str(len(center_x_list1)) + ' ' + str(len(center_x_list2)))
+        
+    return (center_x_list1,center_y_list1,center_x_list2,center_y_list2)
+#try to make clusters that are spread apart
+(center_x_list1,center_y_list1,center_x_list2,center_y_list2) = gen_random_centers(num_classes,1000,0.35);
 
 examples_per_class = 30
 spread = 0.05
