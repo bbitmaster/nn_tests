b73c8c5cbaca3eea1c920648c47074dae75a7dd2
Ben Goodrich <bbitmaster@gmail.com>
2014-03-03 16:36:01 -0500

    created mnist reduced cluster test

---
+++ mnist_train_nonstationary_cluster_subset_params.py
@@ -0,0 +1,87 @@
+#mnist_train_params
+from nnet_toolkit import select_funcs as sf;
+
+#A handy name for this run. The data file will be given this name as
+#<resultsdir><simname><version>.h5py
+simname = 'mnist_train_nonstationary_cluster_subset'
+version = '1.1'
+results_dir = '../results/'
+
+data_dir = '../data/'
+
+use_float32=True
+
+incorrect_target = -1.0
+correct_target = 1.0
+
+num_hidden = 512
+#num_hidden2 = 96
+#num_hidden3 = 500
+
+activation_function='tanh'
+activation_function2='tanh'
+activation_function3='linear_rectifier'
+activation_function_final='tanh'
+
+momentum=0.0
+momentum2=0.0
+momentum3=0.0
+momentum_final = 0.0
+
+maxnorm=None
+maxnorm2=None
+maxnorm3=None
+
+sparse_penalty=None
+sparse_target=None
+
+sparse_penalty2=None
+sparse_target2=None
+
+sparse_penalty3=None
+sparse_target3=None
+
+minibatch_size=256
+
+#per layer learning rates
+learning_rate =       0.01
+learning_rate2 =      0.01
+learning_rate3 =      0.01
+learning_rate_final = 0.01
+
+training_epochs = 3000
+
+shuffle_rate = 50
+
+var_alpha = 0.99
+error_difference_threshold = 3.0
+
+#PCA Reduction factor
+reduce_to = 64
+
+#can be normal or weighted
+#cluster_func =  'cluster_func'
+cluster_func2 = None
+cluster_func3 = None
+
+#do_cosinedistance = True
+
+cluster_speed = 0.0
+clusters_selected = 8
+number_to_replace = 16
+
+dropout=None
+dropout2=None
+dropout3=None
+
+initialization_scheme='glorot'
+initialization_scheme2='glorot'
+initialization_scheme3='glorot'
+
+initialization_constant=1.0
+initialization_constant2=1.0
+initialization_constant3=1.0
+
+random_seed = 4;
+
+save_interval = 10*60; #save every 30 minutes
